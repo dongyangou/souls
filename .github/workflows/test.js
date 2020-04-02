3598b85c79d9fe86b020303777d610c2de46b38c
dongyangou <707257348@qq.com>
2020-04-02 14:03:27 +0800

    git push -u origin master

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -18,3 +18,43 @@ function chiled(){
 function mylove(){
     window.alert("弹窗提示");
 }
+
+
+function sa(){
+    var a,b,c;
+    a= 10;
+    b=20;
+    c=a+b;
+    document.getElementById("sa").innerHTML="c等于"+c
+}
+
+function sb(){
+    var lastname;
+    lastname = "lastname";
+    document.getElementById("sb").innerHTML = lastname;
+}
+
+function sb1(){
+    var lastName;
+    lastName = "lastName";
+    document.getElementById("sb").innerHTML = lastName;
+}
+function sc(){
+    var shuzi;
+    var zimu;
+    var zimu2;
+    shuzi = 3.1415926;
+    zimu = "hei dd";
+    zimu2 = "and you?";
+    var zimu2 = 222;
+    document.getElementById("sc").innerHTML = shuzi+"<br>"+zimu+"<br>"+zimu2;
+    document.getElementById("sc").style.fontSize = "20px";
+}
+
+function sd(){
+    var a = 7,b = 8,c =9;
+    var d =  a%= b;
+    var e = a*= c;
+    var f = a /= b;
+    document.getElementById("sd").innerHTML= "a%=b = " + d+"<br>"+"a*= c =" +e+"<br>"+" a /= b =" +f
+}
