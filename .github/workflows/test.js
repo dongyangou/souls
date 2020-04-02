7d6fc39f9f9152ad58400b7d8147eb811ad45bd0
dongyangou <707257348@qq.com>
2020-04-02 10:26:23 +0800

    git push -u origin master

---
+++ .github/workflows/test.js
@@ -0,0 +1,20 @@
+function s(){
+    document.getElementById("s").innerHTML="外部文件改变字体";
+}
+
+
+function img(){
+    document.getElementById("img").src="F:\VScode\github\souls\.github\workflows\image\a.jpg"
+}
+
+function img1(){
+    document.getElementById("img1").src="F:\VScode\github\souls\.github\workflows\image\b.jpg"
+}
+
+function chiled(){
+    document.getElementById('no').innerHTML='愿意用黑色铅笔在地上画圈圈';
+}
+
+function mylove(){
+    window.alert("弹窗提示");
+}
