24b08b4da86adae8af8a95afae0fb39c8d4e5d4c
dongyangou <707257348@qq.com>
2020-04-18 18:47:51 +0800

    2020-4-18 update by huizhou

--- .github/.js/html.js
+++ .github/.js/html.js
@@ -13,13 +13,53 @@
 // var v = new name();
 // console.log(v.w)
 // 带参数的构造函数
-var v = function(p){
-    'use strict';
-    this.price = p
-}
+// var v = function(p){
+//     'use strict';
+//     this.price = p
+// }
 
-var h = new v(1000);
-console.log(h.price)
+// var h = new v(1000);
+// console.log(h.price)
 
-var h = new v(2000);
-console.log(h.price)
+// var h = new v(2000);
+// console.log(h.price)
+
+// var x  = 2;
+// if (2===x) {
+//     console.log(x);
+// }else {
+//     console.log("错了");
+// }
+
+// var msg;
+// console.log(meg);
+
+// var a = 2;
+// console.log(
+//     a%2 === 0 ? 1 : 2
+// );
+/*function v (){};
+console.log(typeof 123);
+console.log(typeof "123");
+console.log(typeof 0.32);
+console.log(typeof false);
+console.log(typeof v);
+console.log(typeof f);
+var f
+console.log( typeof(f) === "undefined" ? "yes" : "no");
+*/
+
+var meg = "hello word";
+var message = Boolean(meg);
+console.log(message);
+var o = 0;
+var oo = Boolean(o);
+console.log(oo);
+var a = 070;
+console.log(a);
+
+console.log(isNaN());
+
+
+var love = 'myLove';
+console.log(love.toString(2))
